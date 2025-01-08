858db7fe5d35e33e5138bbd947521b38884f813a
Pastofarian <85313258+Pastofarian@users.noreply.github.com>
2025-01-08 20:24:35 +0100

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,7 +1,7 @@
 const db = require('./db');
 
 exports.hellow = (req, res) => {
-    db.db.collection("TEST").findOne({ field:"Hello World" }).then(result => {
+    db.db.collection("Test").findOne({ field:"Hello World!" }).then(result => {
         res.status(201).json(result)
     }).catch(error => {
         console.log(error)
